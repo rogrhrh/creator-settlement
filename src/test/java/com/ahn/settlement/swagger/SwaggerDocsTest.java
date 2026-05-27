2c8be0b684d8dd9ef6fb0b35997b0085431bab27
ahnbs <ahnbs0@gmail.com>
2026-05-27 16:28:17 +0900

    feat: SwaggerConfig 전역 OpenAPI 설정 및 SecurityScheme 등록
    closes #13

---
+++ src/test/java/com/ahn/settlement/swagger/SwaggerDocsTest.java
@@ -0,0 +1,33 @@
+package com.ahn.settlement.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SwaggerDocsTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void apiDocs_반환_성공() throws Exception {
+        mockMvc.perform(get("/v3/api-docs"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.info.title").value("Creator Settlement API"))
+                .andExpect(jsonPath("$.components.securitySchemes.X-User-Role").exists())
+                .andExpect(jsonPath("$.components.securitySchemes.X-User-Id").exists());
+    }
+
+    @Test
+    void swaggerUi_접근_성공() throws Exception {
+        mockMvc.perform(get("/swagger-ui/index.html"))
+                .andExpect(status().is3xxRedirection());
+    }
+}
